718a8ee63309fb0ce608114e65ce6762086b0496
Anushka <Anushka.kodithuwakku.95@gmail.com>
2023-10-29 23:05:58 +1300

    Removed Unnecessary classes

--- SouthernCrossAssessment/Support/Resources.cs
+++ SouthernCrossAssessment/Support/Resources.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace SouthernCrossAssessment.Helpers
 {
-    public static class ErrorMessages
+    public static class Resources
     {
+        public static string RegistrationPageUrl = "prod/users";
         public static string UserAlreadyExists=> "UsernameExistsException: User already exists";
         public static string InvalidPasswordMissingUpperCase => "InvalidPasswordException: Password did not conform with policy: Password must have uppercase characters";
         public static string InvalidPasswordMissingSymbol=> "InvalidPasswordException: Password did not conform with policy: Password must have symbol characters";
         public static string InvalidPasswordMissingLowerCase=> "InvalidPasswordException: Password did not conform with policy: Password must have lowercase characters";
         public static string InvalidPasswordlength => "InvalidParameter: 1 validation error(s) found. - minimum field size of 6, SignUpInput.Password.";
+  
+    
     }
 }
