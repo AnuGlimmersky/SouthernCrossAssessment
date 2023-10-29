368061516a3a5d840a42771d475a0644446b9ad2
Anushka <Anushka.kodithuwakku.95@gmail.com>
2023-10-29 23:07:07 +1300

    Added a method to handle waits

---
+++ SouthernCrossAssessment/Helper/UtilityHelper.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SouthernCrossAssessment.Helper
+{
+    public class UtilityHelper
+    {
+        private IWebDriver driver;
+
+        public UtilityHelper(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void WaitForElement(By element)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(element));
+
+        }
+
+    }
+
+}
